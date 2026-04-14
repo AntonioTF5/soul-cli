526cbbf9aeae0b9486c36b3c207548dd009e2051
afonie <agafonov8@gmail.com>
2026-04-14 18:29:04 +0400

    Add .gitignore, remove node_modules and dist from tracking

--- dist/init.d.ts
+++
@@ -1,8 +0,0 @@
-export interface InitOptions {
-    name?: string;
-    description?: string;
-    tone?: string;
-    interactive?: boolean;
-    output?: string;
-}
-export declare function runInit(opts: InitOptions): Promise<void>;
